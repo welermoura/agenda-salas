0a4f13fd0f05f0e00fd33584c7f496292716363b
welermoura <155203871+welermoura@users.noreply.github.com>
2025-10-24 14:30:00 -0300

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders welcome message', () => {
-  render(<App />);
-  const welcomeElement = screen.getByText(/Bem-vindo à Ferramenta de Gestão de Active Directory/i);
-  expect(welcomeElement).toBeInTheDocument();
-});
