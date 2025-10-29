5dbe1cf4730708a0f9a52afbf3f79f6bd311662f
welermoura <155203871+welermoura@users.noreply.github.com>
2025-10-29 08:51:24 -0300

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders page title', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Monitor de Salas de Reunião/i);
-  expect(titleElement).toBeInTheDocument();
-});
